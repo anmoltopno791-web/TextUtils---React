632c633689b4510a30548fad53903145e8e3a650
Anmol Topno <anmoltopno791@gmail.com>
2026-06-02 14:27:47 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -44,30 +44,30 @@ function App() {
   };
   return (
     <>
-      <Router>
-        <Navbar
-          mode={mode}
-          toggleMode={toggleMode}
-          redMode={redMode}
-          toggleRedMode={toggleRedMode}
-        />
-        <Alert alert={alert} />
-        <div className="container my-4">
-          <Routes>
+      {/* <Router> */}
+      <Navbar
+        mode={mode}
+        toggleMode={toggleMode}
+        redMode={redMode}
+        toggleRedMode={toggleRedMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-4">
+        {/* <Routes>
             <Route exact path="/about" element={<About />} />
             <Route
               exact path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyse below"
-                  mode={mode}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+              element={ */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyse below"
+          mode={mode}
+        />
+      </div>
+      {/* //   }
+            // />
+            // </Routes> */}
+      {/* </Router> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -10,9 +10,9 @@ export default function Navbar(props) {
     >
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <a className="navbar-brand" href="#">
             Textutils
-          </Link>
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -27,63 +27,11 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
                 </a>
               </li>
             </ul>
-            <form className="d-flex mx-4" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success " type="submit">
-                Search
-              </button>
-            </form>
             <div
               className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"} mx-4`}
             >
