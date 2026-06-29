d2309bae4ac4a789c10d5079786a05f7b75dffff
Anmol Topno <anmoltopno791@gmail.com>
2026-06-29 16:08:33 +0530

    Fix some issues in textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -113,7 +113,7 @@ export default function TextForm(props) {
       >
         <h2>Your text Summary</h2>
         <p>
-          {text.split(" ").filter((element) => {return element.length !== 0}).length} words and{" "}
+          {text.split(/\s+/).filter((element) => {return element.length !== 0}).length} words and{" "}
           {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").filter((element) => {return element.length !== 0}).length} Minutes read</p>
